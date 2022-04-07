8b1ddac4b86f43daf87cc6c2a0bf1b4a868d6c2a
Theo FoxPVP <FoxPVP27ST@gmail.com>
2022-04-07 16:44:58 +0200

    yes

--- js/code.js
+++ js/code.js
@@ -2,8 +2,8 @@ function getValue() {
 
     var inputID = document.getElementById("id").value;
     var inputMDP = document.getElementById("mdp").value;
-    if (inputID == "admin" && inputMDP == "admin") {
-        alert("Mathilde");
+    if (inputID == "753951" && inputMDP == "8426") {
+        alert("Bienvenue " + inputID);
     }  
     else {
         alert("Utilisateur ou mot de passe incorrect");
